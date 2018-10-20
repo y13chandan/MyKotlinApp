52d0e84d42bba3ab730dc3d3c70d266767b4c794
root <gridle.io>
2018-10-21 00:28:17 +0530

    intent for next activity

--- app/src/main/java/com/example/root/mykotlinapp/activities/SecondActivity.kt
+++ app/src/main/java/com/example/root/mykotlinapp/activities/SecondActivity.kt
@@ -1,12 +1,13 @@
-package com.example.root.mykotlinapp
+package com.example.root.mykotlinapp.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.root.mykotlinapp.R
 
-class MainActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_second)
     }
 }
